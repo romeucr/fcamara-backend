94f436a36f71c75cf8f970c9a23b2e1514bdba3b
Thales Baliero Takao <engc.thales@gmail.com>
2021-03-31 06:37:06 -0300

    hotfix: solving conflict

--- backend-school/src/main/java/br/com/fcamara/service/impl/AuthService.java
+++ backend-school/src/main/java/br/com/fcamara/service/impl/AuthService.java
@@ -1,7 +1,5 @@
 package br.com.fcamara.service.impl;
 
-<<<<<<< HEAD
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
